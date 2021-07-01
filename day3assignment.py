37e4bea96a2e40d8876ce53d292d99a21a880aab
Sowmya-0463 <86717084+Sowmya-0463@users.noreply.github.com>
2021-07-01 19:41:13 +0530

    Add files via upload

---
+++ day3assignment.py
@@ -0,0 +1,17 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  6 2021, 13:40:21) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("python calculator")
+python calculator
+>>> a=20
+>>> b=6
+>>> print(a+b)
+26
+>>> print(a-b)
+14
+>>> print(a*b)
+120
+>>> print(a/b)
+3.3333333333333335
+>>> print(a%b)
+2
+>>> 
